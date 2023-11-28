148979022df4dc751a658fb1b5172ec379cc80fe
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-11-28 18:38:43 -0500

    fix glob pattern

--- packages/vite-plugin-tailwind-purgecss/src/index.ts
+++ packages/vite-plugin-tailwind-purgecss/src/index.ts
@@ -1,7 +1,6 @@
 import { PurgeCSS } from 'purgecss';
 import { defaultExtractor } from './extractors/default-extractor';
 import { walk } from 'estree-walker';
-import { join } from 'path';
 import type { ResolvedConfig, Plugin } from 'vite';
 import type { ComplexSafelist, StringRegExpArray, UserDefinedOptions } from 'purgecss';
 
@@ -91,7 +90,8 @@ export function purgeCss(purgeOptions?: PurgeOptions): Plugin {
 			for (const [fileName, asset] of Object.entries(assets)) {
 				const purgeCSSResult = await new PurgeCSS().purge({
 					...purgeOptions,
-					content: [join(viteConfig.root, '**/*.html'), ...(purgeOptions?.content ?? [])],
+					// ideally, we'd pull the content from the TW config
+					content: ['./**/*.html', ...(purgeOptions?.content ?? [])],
 					css: [{ raw: (asset.source as string).trim(), name: fileName }],
 					safelist: {
 						...purgeOptions?.safelist,
