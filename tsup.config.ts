7d9a81a37b717f9ac06cde95feabb5fd0549d324
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-12-15 11:33:55 -0500

    remove cjs support

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 	splitting: true,
 	sourcemap: true,
 	clean: true,
-	format: ['esm', 'cjs'],
+	format: ['esm'],
 	dts: true,
 	outDir: 'dist',
 	skipNodeModulesBundle: false,
